0f884ff460892fb7a6a10ffc3891734c250d202f
jamesmcgill <jamesmcgill@users.noreply.github.com>
2019-01-27 14:19:42 +0100

    Random: reduce and cleanup lines of code in SIMD random generator

--- win32-raytracer/RayTracer.cpp
+++ win32-raytracer/RayTracer.cpp
@@ -5,18 +5,10 @@
 
 constexpr float EPSILON = 0.00001f;
 
-__declspec(align(16)) static const unsigned int INT_MAX_VEC[4]
-  = {INT_MAX, INT_MAX, INT_MAX, INT_MAX};
-__declspec(align(16)) static const __m128i INT_MAX_VEC_MM = _mm_load_si128(
-  (__m128i*)INT_MAX_VEC);
-__declspec(align(16)) static const __m128 F_MAX_VEC_MM = _mm_cvtepi32_ps(
-  INT_MAX_VEC_MM);
-
-__declspec(align(16)) static const float VEC_UNIT[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-__declspec(align(16)) static const __m128 VEC_UNIT_MM = _mm_load_ps(VEC_UNIT);
-
-__declspec(align(16)) static const float VEC_HALF[4] = {0.5f, 0.5f, 0.5f, 0.5f};
-__declspec(align(16)) static const __m128 VEC_HALF_MM = _mm_load_ps(VEC_HALF);
+static const __m128i INT_MAX_VEC_MM = _mm_set1_epi32(INT_MAX);
+static const __m128 F_MAX_VEC_MM    = _mm_cvtepi32_ps(INT_MAX_VEC_MM);
+static const __m128 VEC_UNIT_MM     = _mm_set1_ps(1.0f);
+static const __m128 VEC_HALF_MM     = _mm_set1_ps(0.5f);
 
 namespace ptr
 {
@@ -30,24 +22,13 @@ public:
   // https://software.intel.com/en-us/articles/fast-random-number-generator-on-the-intel-pentiumr-4-processor/
   inline void rand_sse(float* result)
   {
-    __declspec(align(16)) __m128i cur_seed_split;
-    __declspec(align(16)) __m128i multiplier;
-    __declspec(align(16)) __m128i adder;
-    __declspec(align(16)) __m128i mod_mask;
-    __declspec(align(16)) __m128i sra_mask;
-    __declspec(align(16)) static const unsigned int mult[4]
-      = {214013, 17405, 214013, 69069};
-    __declspec(align(16)) static const unsigned int gadd[4]
-      = {2531011, 10395331, 13737667, 1};
-    __declspec(align(16)) static const unsigned int mask[4]
-      = {0xFFFFFFFF, 0, 0xFFFFFFFF, 0};
-    __declspec(align(16)) static const unsigned int masklo[4]
-      = {0x00007FFF, 0x00007FFF, 0x00007FFF, 0x00007FFF};
-    adder          = _mm_load_si128((__m128i*)gadd);
-    multiplier     = _mm_load_si128((__m128i*)mult);
-    mod_mask       = _mm_load_si128((__m128i*)mask);
-    sra_mask       = _mm_load_si128((__m128i*)masklo);
-    cur_seed_split = _mm_shuffle_epi32(cur_seed, _MM_SHUFFLE(2, 3, 0, 1));
+    __m128i adder      = _mm_setr_epi32(2531011, 10395331, 13737667, 1);
+    __m128i multiplier = _mm_setr_epi32(214013, 17405, 214013, 69069);
+    __m128i mod_mask   = _mm_setr_epi32(0xFFFFFFFF, 0, 0xFFFFFFFF, 0);
+    __m128i sra_mask   = _mm_set1_epi32(0x00007FFF);
+    __m128i cur_seed_split
+      = _mm_shuffle_epi32(cur_seed, _MM_SHUFFLE(2, 3, 0, 1));
+
     cur_seed       = _mm_mul_epu32(cur_seed, multiplier);
     multiplier     = _mm_shuffle_epi32(multiplier, _MM_SHUFFLE(2, 3, 0, 1));
     cur_seed_split = _mm_mul_epu32(cur_seed_split, multiplier);
@@ -58,19 +39,18 @@ public:
     cur_seed       = _mm_add_epi32(cur_seed, adder);
 
     // CUSTOM CODE for returning floats in range [0 -> 1)
-    __declspec(align(16)) __m128 realConversion;
-    realConversion = _mm_cvtepi32_ps(cur_seed);
+    __m128 realConversion = _mm_cvtepi32_ps(cur_seed);
+    realConversion        = _mm_div_ps(realConversion, F_MAX_VEC_MM);
+    realConversion        = _mm_add_ps(realConversion, VEC_UNIT_MM);
+    realConversion        = _mm_mul_ps(realConversion, VEC_HALF_MM);
 
-    realConversion = _mm_div_ps(realConversion, F_MAX_VEC_MM);
-    realConversion = _mm_add_ps(realConversion, VEC_UNIT_MM);
-    realConversion = _mm_mul_ps(realConversion, VEC_HALF_MM);
     _mm_store_ps(result, realConversion);
 
     return;
   }
 
 private:
-  __declspec(align(16)) __m128i cur_seed;
+  __m128i cur_seed;
 
   void srand_sse(unsigned int seed)
   {
